454aa7aed53afdbad2c8c91fad99887cc063c72b
Sokhanakupe <sokhanakupe@gmail.com>
2026-05-04 21:39:06 +0200

    modified:   app.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const colors = ["green", "red", "rgba(133,122,200)","#f1f15025"];
-const btn = document.getElementById('btn');
-const colo =document.querySelector(".color");
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
 btn.addEventListener("click",function () {
-    console.log(document.body);
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
 
-    const randomNumber =2;
+    
 document.body.style.backgroundColor = colors[randomNumber];
-colors.textContent = colors[randomNumber]
+color.textContent = colors[randomNumber];
 });
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length );
+}
